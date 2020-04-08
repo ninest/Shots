bd5593ef3522a6e080fc90666b01e9b91674dd36
themindstorm <parth.kabra@gmail.com>
2020-04-09 00:58:38 +0800

    plugged in router

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,13 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:shots/router/router.gr.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // home: HomeR
+      builder: ExtendedNavigator<Router>(router: Router()),
     );
   }
 }
---
+++ lib/router/router.gr.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:shots/components/home/home_route.dart';
+
+abstract class Routes {
+  static const homeRoute = '/';
+}
+
+class Router extends RouterBase {
+  //This will probably be removed in future versions
+  //you should call ExtendedNavigator.ofRouter<Router>() directly
+  static ExtendedNavigatorState get navigator =>
+      ExtendedNavigator.ofRouter<Router>();
+
+  @override
+  Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+    switch (settings.name) {
+      case Routes.homeRoute:
+        return MaterialPageRoute<dynamic>(
+          builder: (_) => HomeRoute(),
+          settings: settings,
+        );
+      default:
+        return unknownRoutePage(settings.name);
+    }
+  }
+}
