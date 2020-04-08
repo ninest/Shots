10660c0a6c5466cfb379a5648f5feb9be978b230
themindstorm <parth.kabra@gmail.com>
2020-04-09 01:00:36 +0800

    added page template

---
+++ lib/components/core/page_template.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class PageTemplate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    double statusBarHeight = MediaQuery.of(context).padding.top;
+
+    return Container(
+      // padding: EdgeInsets.all()
+    );
+  }
+}
