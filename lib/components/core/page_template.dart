ca25d9d1bd085f52b1785c2705ca5f9079b6b210
themindstorm <parth.kabra@gmail.com>
2020-04-09 01:04:05 +0800

    added Spacing widget

--- lib/components/core/page_template.dart
+++ lib/components/core/page_template.dart
@@ -1,14 +1,20 @@
-
 import 'package:flutter/material.dart';
+import 'package:shots/styles/values.dart';
 
 class PageTemplate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     double statusBarHeight = MediaQuery.of(context).padding.top;
 
     return Container(
-      // padding: EdgeInsets.all()
+      padding: EdgeInsets.symmetric(
+        horizontal: Values.pageHorizontalPadding,
+      ),
+      child: CustomScrollView(
+        slivers: <Widget>[
+          
+        ]
+      )
     );
   }
 }
