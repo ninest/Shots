ed29ec30eaed8b19099b64b7812aacf2e6b91b50
themindstorm <parth.kabra@gmail.com>
2020-04-09 01:01:40 +0800

    added values.dart

---
+++ lib/styles/values.dart
@@ -0,0 +1,12 @@
+class Values {
+  /*
+  basic font size
+  used in search boxes and buttons mainly
+  */
+  static double get em => 22;
+
+  /*
+  padding for main pageTemplate
+  */
+  static double get pageHorizontalPadding => 20;
+}
