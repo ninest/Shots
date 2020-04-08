ccea4cea296fe1d6fcc55400b1932e06c886f9ad
themindstorm <parth.kabra@gmail.com>
2020-04-09 01:16:19 +0800

    started theme.dart

--- lib/components/home/home_route.dart
+++ lib/components/home/home_route.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:shots/components/core/page_template.dart';
 
 class HomeRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return PageTemplate(children: <Widget>[]);
   }
+
+  Widget appTitle() => Column(
+        children: <Widget>[],
+      );
 }
---
+++ lib/styles/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shots/styles/values.dart';
+
+// final ThemeData appLightTheme = _buildLightTheme();
+
+TextTheme _buildTextTheme(TextTheme base) {
+  double fontSize = Values.em;
+  return base.copyWith(
+    // title: 
+  );
+}
