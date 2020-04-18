c24242d8013a3c7ceceeaf50bdd8658c02e63a9f
themindstorm <parth.kabra@gmail.com>
2020-04-18 19:30:52 +0800

    added developer card pack

--- lib/app/utils/packs.dart
+++ lib/app/utils/packs.dart
@@ -3,10 +3,11 @@ import 'package:shots/app/models/pack_model.dart';
 class Packs {
   static PackModel get basic => PackModel(name: 'Basic', filename: 'basic', selected: false);
   static PackModel get close => PackModel(name: 'Close friends only', filename: 'close');
+  static PackModel get dev => PackModel(name: 'Developers', filename: 'developer');
 
   // un comment below for testing purposes
   static PackModel get test => PackModel(name: 'Test', filename: 'test', selected: true);
 
   // all packs
-  static List<PackModel> get all => [basic, close, test];
+  static List<PackModel> get all => [basic, close, dev, test];
 }
