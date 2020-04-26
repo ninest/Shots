cfe76e6b5f0f471f683e84b9ca56b018c03e0374
themindstorm <parth.kabra@gmail.com>
2020-04-26 13:01:30 +0800

    added comments in more places

--- lib/app/styles/values.dart
+++ lib/app/styles/values.dart
@@ -1,22 +1,20 @@
 class Values {
-  /*
-  basic font size
-  used in search boxes and buttons mainly
-  */
+  /// Basic font size, used in search boxes and buttons mainly
   static double get em => 19;
 
+  /// Padding used when a specified padding is not known
   static double get mainPadding => 33;
-  /*
-  padding for main pageTemplate
-  */
+
+  ///padding for main pageTemplate
   static double get pageHorizontalPadding => mainPadding;
 
-  // button padding
+  /// button padding
   static double get buttonHorizontalPadding => 25.0;
   static double get buttonVerticalPadding => 13.0;
 
+  /// Border radius to use app-wide
   static double get borderRadius => 10.0;
 
-  // opacity uses app-wide
+  /// opacity uses app-wide
   static double get containerOpacity => 0.3;
 }
