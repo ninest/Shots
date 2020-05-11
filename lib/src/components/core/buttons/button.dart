d1fe5bf08339b950b266995161b1b86fac1e4233
ninest <parth.kabra@gmail.com>
2020-05-11 15:35:20 +0800

    added comemnt

--- lib/src/components/core/buttons/button.dart
+++ lib/src/components/core/buttons/button.dart
@@ -74,6 +74,7 @@ class Button extends StatelessWidget {
 
     if (outline) ts = TextStyles.button.c(color);
 
+    // increased font size of focus
     if (focus) ts = TextStyles.button.s(1.7 * Values.em);
 
     return Text(
