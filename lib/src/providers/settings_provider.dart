a7413373da16b3cdfb2561cd8717d776a7ee4f2c
ninest <parth.kabra@gmail.com>
2020-05-15 13:32:44 +0800

    added default for high performance animations

--- lib/src/providers/settings_provider.dart
+++ lib/src/providers/settings_provider.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:shots/src/constants/hive_strings.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 class SettingsProvider extends ChangeNotifier {
   loadSettings() {
     print("Loading settings ...");
     final settingsBox = Hive.box(HiveBoxes.settings);
     nsfw = settingsBox.get(SettingsBox.nsfw, defaultValue: false);
+
+    // if iphone, default for [highPerformanceAnimation] should be true
+    bool defaultValue = UniversalPlatform.isIOS;
     highPerformanceAnimation =
-        settingsBox.get(SettingsBox.highPerformanceAnimation, defaultValue: false);
+        settingsBox.get(SettingsBox.highPerformanceAnimation, defaultValue: defaultValue);
   }
 
   bool nsfw;
@@ -17,7 +21,6 @@ class SettingsProvider extends ChangeNotifier {
 
 class SettingsService {
   static var _settingsBox = Hive.box(HiveBoxes.settings);
-
 
   // todo: refactor these functins and shorten them
   static enableNsfw() {
